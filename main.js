6330cad86f94f73b648fe77f019a70bdbc4f399a
waysideoff <alildevop@gmail.com>
2020-10-22 17:05:39 -0400

    uploaded new book covers

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ let images = [];
 let time = 5000;
 
 //Image List
-images[0] = 'img/book-covers/book1.jpg';
-images[1] = 'img/book-covers/book2.jpg';
-images[2] = 'img/book-covers/book3.jpg';
-images[3] = 'img/book-covers/book4.jpg';
+images[0] = 'img/book-covers/main/book1.jpg';
+images[1] = 'img/book-covers/main/book2.jpg';
+images[2] = 'img/book-covers/main/book3.jpg';
+images[3] = 'img/book-covers/main/book4.jpg';
 
 //Change Image
 
